fc3a6e57744955ef4e58cda972152dfc2be4f18e
Armando Tomazzoni Jr <armando.junior@luizalabs.com>
2022-11-02 16:23:50 -0300

    🥇 done

--- pages/api/email.ts
+++ pages/api/email.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+import fs from 'fs'
 import { readFile } from 'fs/promises'
 
 import formData from 'form-data'
@@ -26,48 +27,53 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseProps>
 ) {
-  // 1 - Setup Mailgun library
-  const mailgun = new Mailgun(formData)
-  const mg = mailgun.client({
-    username: 'api',
-    key: process.env.MAILGUN_API_KEY || 'or_put_your_api_key_here',
-  })
-
-  // 2 - Setup Multiparty library
-  const form = new multiparty.Form({
-    uploadDir: './public/uploads/',
-  })
-
-  // 2.1 - Get data from request
-  const data: any = await new Promise((resolve, reject) => {
-    form.parse(req, function (err, fields, files) {
-      if (err) {
-        res.status(400).json({ status: 500, message: err.message })
-        reject({ err })
-      }
-      resolve({ fields, files })
-    })
-  })
-
-  // 3 - Setup required fields
-  const domain = 'api.agenciaade.com.br'
-  const fromEmail = `${data.fields.as[0]} <api@agenciaade.com.br>`
-  const replyToEmail = `${data.fields.as[0]} <${data.fields.from[0]}>`
-  const toEmails = data.fields.to
-  const subject = data.fields.subject[0]
-  const message = data.fields.message[0]
-
-  // 4 - Setup optional fields
-  const ccEmails = data.fields.cc ? data.fields.cc : []
-  const bccEmails = data.fields.bcc ? data.fields.bcc : []
-  const attachFile = data.files.attach
-    ? {
-        filename: data.files.attach[0].originalFilename,
-        data: await readFile(data.files.attach[0].path),
-      }
-    : {}
-
   try {
+    // 1 - Setup Mailgun library
+    const mailgun = new Mailgun(formData)
+    const mg = mailgun.client({
+      username: 'api',
+      key: process.env.MAILGUN_API_KEY || 'or_put_your_api_key_here',
+    })
+
+    // 2 - Setup Multiparty library
+    if (!fs.existsSync('./public/uploads')) {
+      fs.mkdirSync('./public/uploads')
+    }
+    const form = new multiparty.Form({
+      uploadDir: './public/uploads/',
+    })
+
+    // 2.1 - Get data from request
+    const data: any = await new Promise((resolve, reject) => {
+      form.parse(req, function (err, fields, files) {
+        if (err) {
+          res.status(400).json({ status: 500, message: err.message })
+          reject({ err })
+        }
+        resolve({ fields, files })
+      })
+    })
+
+    // 3 - Setup required fields
+    const domain = 'api.agenciaade.com.br'
+    const fromEmail = `${data.fields.as[0]} <api@agenciaade.com.br>`
+    const replyToEmail = `${data.fields.as[0]} <${data.fields.from[0]}>`
+    const toEmails = data.fields.to
+    const subject = data.fields.subject[0]
+    const message = data.fields.message[0]
+
+    // 4 - Setup optional fields
+    const ccEmails = data.fields.cc ? data.fields.cc : []
+    const bccEmails = data.fields.bcc ? data.fields.bcc : []
+    const attachFile = data.files.attach
+      ? [
+          {
+            filename: data.files.attach[0].originalFilename,
+            data: await readFile(data.files.attach[0].path),
+          },
+        ]
+      : null
+
     // Wrong http method
     if (req.method !== 'POST') {
       res.status(400).json({ status: 400, message: 'Not found' })
@@ -83,14 +89,14 @@ export default async function handler(
       subject: subject,
       html: message,
       text: message.replace(/(<([^>]+)>)/gi, ''),
-      attachment: [attachFile],
+      attachment: attachFile,
     })
     res.status(200).json({
       status: sendResult.status,
       message: sendResult.message,
     })
   } catch (error: any) {
-    res.status(400).json({ status: 400, message: error })
+    res.status(400).json({ status: 400, message: error.message })
   }
 }
 
