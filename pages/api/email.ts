fb65e746d1aeeb6a1e993369cda79ff7e6e84277
Armando Tomazzoni Jr <armando.junior@luizalabs.com>
2022-11-04 13:39:08 -0300

    👾 fix next cors

--- pages/api/email.ts
+++ pages/api/email.ts
@@ -18,6 +18,18 @@ export default async function handler(
     return false
   }
 
+  // Cors
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+  )
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+
   try {
     // 1 - Setup Mailgun library
     const mailgun = new Mailgun(formData)
